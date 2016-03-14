429cff72337d4661ab776ae8a07fa5bcd0b6dc29
Steve Lam <stevyo99@yahoo.ca>
2016-03-14 15:53:24 -0700

    consolidated the service test classes into a single parameterized test class

--- src/test/java/ca/douglascollege/flamingdodos/realestate/data/services/BaseServiceTest.java
+++ src/test/java/ca/douglascollege/flamingdodos/realestate/data/services/BaseServiceTest.java
@@ -1,19 +1,174 @@
 package ca.douglascollege.flamingdodos.realestate.data.services;
 
+import ca.douglascollege.flamingdodos.database.annotations.SqliteColumn;
 import ca.douglascollege.flamingdodos.database.models.BaseModel;
 import ca.douglascollege.flamingdodos.database.services.BaseService;
+import ca.douglascollege.flamingdodos.realestate.data.models.AgentModel;
+import ca.douglascollege.flamingdodos.realestate.data.models.CustomerModel;
+import ca.douglascollege.flamingdodos.realestate.data.models.PropertyListingModel;
+import ca.douglascollege.flamingdodos.realestate.data.models.SaleTransactionModel;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.tmatesoft.sqljet.core.table.SqlJetDb;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
-public abstract class BaseServiceTest<T extends BaseModel> {
+@RunWith(Parameterized.class)
+public class BaseServiceTest<T extends BaseModel> {
+    @Parameterized.Parameters(name = "{3}")
+    public static Collection<Object[]> data() {
+        List<Object[]> parameters = new ArrayList<>();
+
+        BaseModel evalModel;
+        BaseModel phonyModel;
+
+        {
+            evalModel = new AgentModel();
+            phonyModel = new AgentModel();
+
+            ((AgentModel) evalModel).firstName = "Victor";
+            ((AgentModel) phonyModel).firstName = "Simon";
+
+            ((AgentModel) evalModel).lastName = "Choong";
+            ((AgentModel) phonyModel).lastName = "Liiii";
+
+            ((AgentModel) evalModel).hireDate = new Date(1457993215547L);
+            ((AgentModel) phonyModel).hireDate = new Date(1448732418975L);
+
+            parameters.add(new Object[]{AgentService.class, evalModel, phonyModel, "Agent"});
+        }
+
+        {
+            evalModel = new CustomerModel();
+            phonyModel = new CustomerModel();
+
+            ((CustomerModel) evalModel).firstName = "Donald";
+            ((CustomerModel) phonyModel).firstName = "Ronald";
+
+            ((CustomerModel) evalModel).lastName = "Trump";
+            ((CustomerModel) phonyModel).lastName = "Reagan";
+
+            parameters.add(new Object[]{CustomerService.class, evalModel, phonyModel, "Customer"});
+        }
+
+        {
+            evalModel = new PropertyListingModel();
+            phonyModel = new PropertyListingModel();
+
+            ((PropertyListingModel) evalModel).agentId = 4;
+            ((PropertyListingModel) phonyModel).agentId = 1;
+
+            ((PropertyListingModel) evalModel).customerId = 5;
+            ((PropertyListingModel) phonyModel).customerId = 2;
+
+            ((PropertyListingModel) evalModel).askingPrice = 657432.48;
+            ((PropertyListingModel) phonyModel).askingPrice = 1574357.67;
+
+            ((PropertyListingModel) evalModel).propertyType = PropertyListingModel.PropertyType.RESIDENTIAL;
+            ((PropertyListingModel) phonyModel).propertyType = PropertyListingModel.PropertyType.COMMERCIAL;
+
+            ((PropertyListingModel) evalModel).buildingType = PropertyListingModel.BuildingType.SINGLE_FAMILY_HOME;
+            ((PropertyListingModel) phonyModel).buildingType = PropertyListingModel.BuildingType.OTHER;
+
+            ((PropertyListingModel) evalModel).address = "700 Royal Avenue, New Westminister, BC, V3M 5Z5";
+            ((PropertyListingModel) phonyModel).address = "1250 Pinetree Way, Coquitlam, BC, V3B 7X3";
+
+            ((PropertyListingModel) evalModel).floorArea = 2687.2;
+            ((PropertyListingModel) phonyModel).floorArea = 8561.8;
+
+            ((PropertyListingModel) evalModel).landArea = 2973.7;
+            ((PropertyListingModel) phonyModel).landArea = 9065.5;
+
+            ((PropertyListingModel) evalModel).bedroomCount = 2;
+            ((PropertyListingModel) phonyModel).bedroomCount = 6;
+
+            ((PropertyListingModel) evalModel).bathroomCount = 2;
+            ((PropertyListingModel) phonyModel).bathroomCount = 4;
+
+            ((PropertyListingModel) evalModel).buildYear = 2004;
+            ((PropertyListingModel) phonyModel).buildYear = 2011;
+
+            ((PropertyListingModel) evalModel).status = PropertyListingModel.PropertyStatus.FOR_SALE;
+            ((PropertyListingModel) phonyModel).status = PropertyListingModel.PropertyStatus.SOLD;
+
+            parameters.add(new Object[]{PropertyListingService.class, evalModel, phonyModel, "PropertyListing"});
+        }
+
+        {
+            evalModel = new SaleTransactionModel();
+            phonyModel = new SaleTransactionModel();
+
+            ((SaleTransactionModel) evalModel).listingId = 7;
+            ((SaleTransactionModel) phonyModel).listingId = 3;
+
+            ((SaleTransactionModel) evalModel).buyerId = 3;
+            ((SaleTransactionModel) phonyModel).buyerId = 6;
+
+            ((SaleTransactionModel) evalModel).date = new Date(1457987387657L);
+            ((SaleTransactionModel) phonyModel).date = new Date(1448675435795L);
+
+            ((SaleTransactionModel) evalModel).amount = 897535.68;
+            ((SaleTransactionModel) phonyModel).amount = 685745.76;
+
+            parameters.add(new Object[]{SaleTransactionService.class, evalModel, phonyModel, "SaleTransaction"});
+        }
+
+        return parameters;
+    }
+
     private SqlJetDb mDatabase;
     private BaseService<T> mService;
 
-    protected abstract BaseService<T> createService(SqlJetDb db);
+    private Class<? extends BaseService<T>> mServiceClass;
+    private T mEvaluationModel;
+    private T mPhonyModel;
+
+    public BaseServiceTest(Class<? extends BaseService<T>> serviceClass, T evaulationModel, T phonyModel, String name) {
+        mServiceClass = serviceClass;
+        mEvaluationModel = evaulationModel;
+        mPhonyModel = phonyModel;
+    }
+
+    protected BaseService<T> createService(SqlJetDb db) throws Exception {
+        return mServiceClass.getConstructor(SqlJetDb.class).newInstance(db);
+    }
+
+    protected T populateModelWithEvaulatableData(T model) throws Exception {
+        return populateModelWithData(model, mEvaluationModel);
+    }
+
+    protected T populateModelWithPhonyData(T model) throws Exception {
+        return populateModelWithData(model, mPhonyModel);
+    }
+
+    private T populateModelWithData(T model, T reference) throws Exception {
+        for (Field f : model.getClass().getDeclaredFields()) {
+            SqliteColumn column = f.getAnnotation(SqliteColumn.class);
+            if (column != null) {
+                f.set(model, f.get(reference));
+            }
+        }
+
+        return model;
+    }
+
+    protected void evaluateModelData(T model) throws Exception {
+        for (Field f : model.getClass().getDeclaredFields()) {
+            SqliteColumn column = f.getAnnotation(SqliteColumn.class);
+            if (column != null) {
+                assertEquals(f.get(model), f.get(mEvaluationModel));
+            }
+        }
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -85,10 +240,4 @@ public abstract class BaseServiceTest<T extends BaseModel> {
     public void testNewModel() throws Exception {
         assertNotNull(mService.newModel());
     }
-
-    protected abstract T populateModelWithEvaulatableData(T model);
-
-    protected abstract T populateModelWithPhonyData(T model);
-
-    protected abstract void evaluateModelData(T model) throws Exception;
 }
